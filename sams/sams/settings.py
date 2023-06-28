67f65be049fe7d13dd81ab05e8c0628f609999f5
Kirubel321 <kirubel1esayas10@gmail.com>
2023-06-28 19:27:05 +0300

    setting updated for forgot email

--- sams/sams/settings.py
+++ sams/sams/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'samsApp.User'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kirubel.who@gmail.com'
+EMAIL_HOST_PASSWORD = 'pnizxkhfeuhcexkv'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'kirubel.who@gmail.com'
